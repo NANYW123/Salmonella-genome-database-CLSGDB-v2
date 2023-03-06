6903d7b956d1aefc9a0e7e840ef66617a0706d92
Yanan Wang <49115736+NANYW123@users.noreply.github.com>
2023-03-06 16:03:14 +0800

    Update 03_Assembly.sh

--- 03_Assembly.sh
+++ 03_Assembly.sh
@@ -12,7 +12,5 @@ mkdir 02_Assembly
 
 $unicycler -1 ${CleanData}/${SampleID}_final_R1.fastq.gz -1 ${CleanData}/${SampleID}_final_R2.fastq.gz -o ./02_Assembly/${SampleID} -t 12
 
-$unicycler --no_pilon --no_correct  --min_kmer_frac 0.6  --max_kmer_frac 0.99  --kmer_count 3  --min_fasta_length 500 -1 $CleanData/${SampleID}_1.fastq.gz -2 CleanData/${SampleID}_2.fastq.gz -o ./spades_out/${SampleID} -t 10
-
 #return to initial directory
 cd ../../
